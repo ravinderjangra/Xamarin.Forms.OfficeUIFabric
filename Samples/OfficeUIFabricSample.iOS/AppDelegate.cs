300c05700da55bf8c8e24909cba25c43c6ffe263
Ravinder Jangra <ravinderjangra@live.com>
2020-02-24 17:06:26 +0530

    update iOS sample to use iOS lib project

--- Samples/OfficeUIFabricSample.iOS/AppDelegate.cs
+++ Samples/OfficeUIFabricSample.iOS/AppDelegate.cs
@@ -22,6 +22,7 @@ namespace OfficeUIFabricSample.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            Visual.Fabric.iOS.FabricVisual.Init();
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
