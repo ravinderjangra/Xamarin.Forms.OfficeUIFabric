98d4487e080eac248f5f7056e426925a7dc8045a
Ravinder Jangra <ravinderjangra@live.com>
2020-05-09 21:11:16 +0530

    expose label and button styles

---
+++ Visual.Fabric/Visual.Fabric.Forms/FabricButton.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace Visual.Fabric.Forms
+{
+    public static class FabricButton
+    {
+        public static readonly BindableProperty ButtonStyleProperty =
+            BindableProperty.CreateAttached(
+                "ButtonStyle",
+                typeof(FabricButtonStyle),
+                typeof(Button),
+                FabricButtonStyle.PrimaryFilled);
+
+        public static FabricButtonStyle GetButtonStyle(BindableObject view)
+        {
+            return (FabricButtonStyle)view.GetValue(ButtonStyleProperty);
+        }
+
+        public static void SetButtonStyle(BindableObject view, FabricButtonStyle value)
+        {
+            view.SetValue(ButtonStyleProperty, value);
+        }
+    }
+}
---
+++ Visual.Fabric/Visual.Fabric.Forms/FabricControlStyles.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Visual.Fabric.Forms
+{
+    public enum FabricButtonStyle
+    {
+        PrimaryFilled,
+        PrimaryOutLine,
+        SecondaryOutLine,
+        Borderless,
+    }
+
+    public enum FabricLabelColorStyle
+    {
+        Regular,
+        Secondary,
+        White,
+        Primary,
+        Error,
+        Warning,
+        Disabled
+    }
+
+    public enum FabricLabelTextStyle
+    {
+        LargeTitle,
+        Title1,
+        Title2,
+        Headline,
+        HeadlineUnscaled,
+        Body,
+        BodyUnscaled,
+        Subhead,
+        Footnote,
+        FootnodeUnscaled,
+        Button1,
+        Button2,
+        Button3,
+        Caption1,
+        Caption2
+    }
+
+    public enum FabricLabelWeightStyle
+    {
+        Bold,
+        Semibold,
+        Medium,
+        Regular
+    }
+}
