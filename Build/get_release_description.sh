464e57c3808fd2396119697df2f46aa7a3a9955f
Ravinder Jangra <ravinderjangra@live.com>
2020-05-10 12:58:19 +0530

    version change to 0.0.1

---
+++ Build/get_release_description.sh
@@ -0,0 +1,31 @@
+read -r -d '' release_description << 'EOF'
+Xamarin.Android, Xamarin.iOS bindings and Xamarin.Forms Visual package for FabricUI.
+
+## Changelog
+CHANGELOG_CONTENT
+
+## NuGet Package SHA-256 checksums:
+```
+Xamarin.Forms.Visual.Fabric
+FORMS_NUGET_PACKAGE_CHECKSUM
+
+Xamarin.Android.FabricComponents
+ANDROID_NUGET_PACKAGE_CHECKSUM
+
+Xamarin.iOS.FabricComponents
+IOS_PACKAGE_CHECKSUM
+```
+
+EOF
+
+commitMessage=$(git log --format=%B -n 1)
+version=$(perl -pe '($_)=/([0-9]+([.][0-9]+)+([-][Rr][Cc][0-9]+)?)/' <<< $commitMessage)
+forms_package_checksum=$(sha256sum "../Xamarin.Forms.Visual.Fabric.${version}.nupkg" | awk '{ print $1 }')
+android_package_checksum=$(sha256sum "../MaidSafe.SafeApp.${version}.nupkg" | awk '{ print $1 }')
+ios_package_checksum=$(sha256sum "../Xamarin.iOS.FabricComponents.${version}.nupkg" | awk '{ print $1 }')
+changelog_content=$(sed '1,/### Changes/d;/##/,$d' ../CHANGELOG.MD)
+release_description=$(sed "s/FORMS_NUGET_PACKAGE_CHECKSUM/$forms_package_checksum/g" <<< "$release_description")
+release_description=$(sed "s/ANDROID_NUGET_PACKAGE_CHECKSUM/$android_package_checksum/g" <<< "$release_description")
+release_description=$(sed "s/IOS_PACKAGE_CHECKSUM/$ios_package_checksum/g" <<< "$release_description")
+
+echo "${release_description/CHANGELOG_CONTENT/$changelog_content}" > release_description.txt
---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Fabric packages Changelog
+
+## [0.0.1]
+
+### Changes
+
+- Expose Fabric Visual styles for Android and iOS.
+- Added button and label control samples using Fabric Visual.
+- Updated iOS bindings to v1.0.6.
+
+## [0.0.1] - RC 1
+
+### Changes
+
+- Initial expose FabricUI Android and iOS bindings.
+- Added Xamarin.Forms Fabric Visual support.
